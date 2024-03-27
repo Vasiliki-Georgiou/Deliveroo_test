bdb68e109f195cc0fc9180440ec971c96e15671e
Vasiliki Georgiou <vicky.georgiou2012@gmail.com>
2024-03-27 21:06:11 +0100

    Removing validation from customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,5 +2,5 @@ class Customer < ApplicationRecord
   has_many :orders
   validates :name, presence: true, length: { minimum: 5 }
   validates :address, presence: true, length: { minimum: 10 }
-  validates :email, presence: true, uniqueness: true, length: { minimum: 5 }, format: URI::MailTo::EMAIL_REGEXP
+  # validates :email, presence: true, uniqueness: true, length: { minimum: 5 }, format: URI::MailTo::EMAIL_REGEXP
 end
