4e50dec5897417a53d7af044527df15056a2234f
Vasiliki Georgiou <vicky.georgiou2012@gmail.com>
2024-03-27 12:11:03 +0100

    Created migrations for username and avatar_url, updated db schema, created seeds for restaurants

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar_url])
+  end
+
+  def current_url(overwrite = {})
+    url_for :only_path => false, :params => params.merge(overwrite)
+  end
 end
